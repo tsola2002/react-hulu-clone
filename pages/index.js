6c355efd4b4dd86ea7adc89bd245a16cebeff283
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2021-05-13 09:14:26 +0100

    results section has been laid out

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
+import requests from '../utils/requests';
 
 
-export default function Home() {
+export default function Home({results}) {
+  //console.log(props);
   return (
     <div>
       <Head>
@@ -18,7 +20,7 @@ export default function Home() {
 
       <Nav />
       
-      <Results />
+      <Results results={results} />
 
     </div>
   )
@@ -26,4 +28,16 @@ export default function Home() {
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+    ).then((res) => res.json());
+
+    return {
+      props: {
+        results: request.results,
+      }
+    }
 }
